46c1e462216ac811cab60b99a4096e6a5cac9fcc
Rohit <rohitgupta7471@gmail.com>
2024-10-20 14:56:47 +0530

    inital commit

--- src/practise/Practice.java
+++
@@ -1,8 +0,0 @@
-package practise;
-
-public class Practice {
-    public static void main(String[] args) {
-        int amt = 9;
-        System.out.println(Math.round((double) amt/10)*10);
-    }
-}
